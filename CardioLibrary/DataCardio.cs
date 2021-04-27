454c7179ef3d740957bda0548c127b182352fdca
miglio000 <gabrielemiglio23@gmail.com>
2021-04-27 13:20:33 +0200

    progetto informatica

---
+++ CardioLibrary/DataCardio.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CardioLibrary
+{
+    public class DataCardio
+    {
+    }
+}
---
+++ DataCardio_Test/DataCardio.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio_Test
+{
+    [TestClass]
+    public class DataCardio
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
